b9c4dc5a14952687f168c5060e090d3881aefcf2
Andrew Brampton <brampton@gmail.com>
2016-10-23 17:17:18 -0700

    Fixed up the inspect tool to accept two arguments, and to use the API after the refactor.

--- inspect/main.go
+++ inspect/main.go
@@ -2,20 +2,15 @@
 package main
 
 import (
+	"bramp.net/dsector/input"
 	"bramp.net/dsector/ufwb"
 	"flag"
 	"fmt"
 	"os"
 )
 
-var (
-	// TODO Change this to be arg1 and arg2, instead of flags
-	grammar = flag.String("grammar", "", "grammar filename")
-	target = flag.String("target", "", "target filename")
-)
-
-func openGrammar() *ufwb.Ufwb {
-	file, err := os.Open(*grammar)
+func openGrammar(grammar string) *ufwb.Ufwb {
+	file, err := os.Open(grammar)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to open grammar %q: %s\n", grammar, err)
 		os.Exit(1)
@@ -34,7 +29,7 @@ func openGrammar() *ufwb.Ufwb {
 	return g
 }
 
-func decode(u *ufwb.Ufwb, f ufwb.File) {
+func decode(u *ufwb.Ufwb, f input.Input) {
 	decoder := ufwb.NewDecoder(u, f)
 	value, err := decoder.Decode()
 	if err != nil {
@@ -51,28 +46,25 @@ func decode(u *ufwb.Ufwb, f ufwb.File) {
 	fmt.Print(str)
 }
 
-func parseFlags() {
-	flag.Parse()
-
-	if grammar == nil || *grammar == "" {
-		fmt.Fprintf(os.Stderr, "Please specify a grammar file\n")
-		os.Exit(1)
-	}
-
-	if target == nil || *target == "" {
-		fmt.Fprintf(os.Stderr, "Please specify a target file\n")
-		os.Exit(1)
-	}
-
-}
-
 func main() {
 
-	parseFlags()
+	flag.Parse()
+	flag.Usage = func() {
+		fmt.Println("inspect [grammar] [target]")
+	}
 
-	g := openGrammar()
+	args := flag.Args()
+	if len(args) < 2 {
+		flag.Usage()
+		os.Exit(1)
+	}
 
-	file, err := ufwb.OpenOSFile(*target)
+	grammar := args[0]
+	target := args[1]
+
+	g := openGrammar(grammar)
+
+	file, err := input.OpenOSFile(target)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to open target %q: %s\n", target, err.Error())
 		return
